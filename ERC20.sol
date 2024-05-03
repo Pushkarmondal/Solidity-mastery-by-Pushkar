da768dce5de0fbf0443fa5865066c36f9718ab04
Pushkar Mondal <116779860+Pushkarmondal@users.noreply.github.com>
2024-05-04 00:18:47 +0530

    Add files via upload

--- ERC20.sol
+++ ERC20.sol
@@ -1,90 +1,75 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-interface IERC20 {
-//Total supply of nft/coins:
-    function totalSupply() external view returns(uint);
+import "./IERC20.sol";
 
-//Balance of various accounts:
-    function balanceOf(address account) external view returns(uint);
+contract ERC20 is IERC20{
 
-//This function Transfer tokens receipent address and amount of tokens:
-    function transfer(address receipent, uint amount) external returns(bool);
+    event Transfer(
+    address indexed from, address indexed to, uint256 value
+    );
 
-//The allowance function tells you how many tokens one person (the "owner") has allowed another person (the "spender") to spend on their behalf. 
-    function allowance(address owner, address spender)
-             external view returns(uint);
+    event Approval(
+        address indexed owner, address indexed spender, uint256 value
+    );
 
-//This function approves the spender that how much he can spend on behalf of owner:   
-    function approve(address spender, uint amount)
-              external returns(bool);
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    address public owner;
 
-//This function is use for transfer tokens by using sender address and receipent address and also total amount:  
-    function transferFrom(
-        address sender,
-        address receipent,
-        uint amount
-    ) external returns(bool);
+    modifier onlyOwner() {
+        msg.sender == owner;
+        _;
+    }
 
-//This event will called when some transfer happened:
-    event Transfer(address indexed from, address indexed to, uint amount);
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+    }
 
-//This event will called when some approval from owner side happened:
-    event Approval(address indexed owner, address indexed spender, uint amount);
-}
-
-contract ERC20 is IERC20 {
-    uint public totalSupply; 
-    mapping (address => uint) public balanceOf;
-    mapping(address => mapping (address => uint)) public allowance;
-    string public name = "Test";
-    string public symbol = "TEST";
-    uint8 public decimals = 18;
-
-    function transfer(address receipent, uint amount) external returns(bool){
+    function transfer(address recipient, uint256 amount)external returns (bool) {
         balanceOf[msg.sender] -= amount;
-        balanceOf[receipent] += amount;
-        emit Transfer(msg.sender, receipent, amount);
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
         return true;
     }
 
-    function approve(address spender, uint amount)
-             external returns(bool) {
+    function approve(address spender, uint256 amount) external returns (bool) {
         allowance[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
-
     }
-    
-    function transferFrom(
-        address sender,
-        address receipent,
-        uint amount
-    ) external returns(bool) {
+
+    function transferFrom(address sender, address recipient, uint256 amount)external returns (bool) {
         allowance[sender][msg.sender] -= amount;
         balanceOf[sender] -= amount;
-        balanceOf[receipent] += amount;
-        emit Transfer(sender, receipent, amount);
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
         return true;
     }
 
-    function _mint(address to, uint amount) internal {
+    function _mint(address to, uint256 amount) internal {
         balanceOf[to] += amount;
         totalSupply += amount;
         emit Transfer(address(0), to, amount);
     }
 
-    function _burn(address from, uint amount) internal {
+    function _burn(address from, uint256 amount) internal {
         balanceOf[from] -= amount;
         totalSupply -= amount;
         emit Transfer(from, address(0), amount);
     }
 
-    function mint(address to, uint amount) external {
+    function mint(address to, uint256 amount) external onlyOwner{
         _mint(to, amount);
     }
 
-    function burn(address from, uint amount) external {
+    function burn(address from, uint256 amount) external onlyOwner{
         _burn(from, amount);
     }
 }
